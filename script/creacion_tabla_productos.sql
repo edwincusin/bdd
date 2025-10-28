3fa1cdc37581c79028c727dae8f98b14fa2dbf94
EDWINCSN <c.edwin32@hotmail.com>
2025-10-27 22:10:08 -0500

    RETO 2 COMPLETO / script de creacion de tablas

---
+++ script/creacion_tabla_productos.sql
@@ -0,0 +1,10 @@
+--crear tabla productos con columnas
+create table productos(
+	codigo int not null,
+	nombre varchar(50) not null,
+	descripcion varchar(200),
+	precio money not null,
+	stock int not null,
+
+	constraint productos_pk primary key (codigo)
+)
